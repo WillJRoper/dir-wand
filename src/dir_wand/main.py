51aef3c18f858669bea5852ccd225a7ba3e86f40
wjr21 <wjr21@sussex.ac.uk>
2024-05-25 00:35:05 +0100

    Implemented running logic in main

--- src/dir_wand/main.py
+++ src/dir_wand/main.py
@@ -1,9 +1,11 @@
 """The main runner for Directory WAND
 """
 from dir_wand.parser import Parser
+from dir_wand.template import Template
 
 
 def main():
+    """Wave the Directory WAND and make magic happen."""
     parser = Parser(
         description="Wave your Directory WAND and make magic happen."
     )
@@ -16,3 +18,13 @@ def main():
     print("Root:", args.root)
     for key, value in args.replacements.items():
         print(f"Replacement: {key} = {value}")
+
+    # Create the template
+    template = Template(args.template, **args.replacements)
+    print(template)
+
+    # Make the copy
+    template.make_copies(args.root)
+
+    # Report we're done
+    print("Done!")
