0017b4a5e65a9e9b459c01a7f5e5755688c69e84
wjr21 <wjr21@sussex.ac.uk>
2024-05-27 13:56:57 +0100

    Changed ASCII art

--- src/dir_wand/art.py
+++ src/dir_wand/art.py
@@ -1,9 +1,26 @@
 """A module containing simple ASCII art for Directory WAND."""
 
-ASCII_ART = (
+ASCII_ART_old = (
     "     _______    _______    _______    _______ \n"
     "    /      /,  /      /,  /      /,  /      /,\n"
     "   /  W   //  /  A   //  /  N   //  /  D   // \n"
     "  /______//  /______//  /______//  /______//  \n"
     " (______(/  (______(/  (______(/  (______(/   \n"
+)
+
+ASCII_ART = (
+    "  └── WAND/                     \n"
+    "       ├── WAND/                \n"
+    "       │   ├── WAND/            \n"
+    "       │   │   ├── WAND/        \n"
+    "       │   │   │   └── ...      \n"
+    "       │   │   ├── Automates/   \n"
+    "       │   │   ├── Nested/      \n"
+    "       │   │   └── Directories/ \n"
+    "       │   ├── Automates/       \n"
+    "       │   ├── Nested/          \n"
+    "       │   └── Directories/     \n"
+    "       ├── Automates/           \n"
+    "       ├── Nested/              \n"
+    "       └── Directories/         \n"
 )
